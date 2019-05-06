9fcc450fd3528b899ea37b51ce74ff76071d65df
qss <doublesan@126.com>
2019-05-06 07:09:02 +0000

    填充用户和话题数据

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,11 +15,14 @@ use Faker\Generator as Faker;
 */
 
 $factory->define(App\Models\User::class, function (Faker $faker) {
+    $date_time = $faker->date . ' ' . $faker->time;
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => Str::random(10),
+        'password' => '$2y$10$OP8.0GaIZyBzlNRjM7olYeUe7JYv7w8DjYUcm4fST0ddrnD7NiInC', // 123456
+        'introduction' => $faker->sentence(),
+        'created_at' => $date_time,
+        'updated_at' => $date_time,
     ];
 });
