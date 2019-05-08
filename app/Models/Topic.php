7c01188c06174e47427e2485694b48f15df8104d
qss <doublesan@126.com>
2019-05-08 06:43:21 +0000

    翻译 URL 标示

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -8,7 +8,7 @@ class Topic extends Model
     protected $fillable = [
         'title', 'body', 'category_id', 'excerpt', 'slug'
     ];
-    
+
     public function category()
     {
         return $this->belongsTo(Category::class);
@@ -46,5 +46,10 @@ class Topic extends Model
     {
         // 按照创建时间排序
         return $query->orderBy('created_at', 'desc');
+    }
+
+    public function link($params = [])
+    {
+        return route('topics.show', array_merge([$this->id, $this->slug], $params));
     }
 }
