db6322a88509e9b8c8065b76fd546b2fd364f36c
qss <doublesan@126.com>
2020-03-13 06:31:24 +0000

    添加允许使用的顶级域名

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -22,10 +22,6 @@ class CategoriesController extends Controller
     //     return view('topics.index', compact('topics', 'category'));
     // }
 
-    public function show(Category $category, Request $request, Project $project)
-    {
-    	return view('projects.list', compact('projects', 'category'));
-    }
     public function show(Category $category, Request $request, Topic $topic, User $user)
     {
         // 读取分类 ID 关联的话题，并按每 20 条分页
@@ -38,5 +34,10 @@ class CategoriesController extends Controller
                  
         // 传参变量话题和分类到模板中
         return view('topics.index', compact('topics', 'category', 'active_users'));
+    }
+
+    public function show(Category $category, Request $request, Project $project)
+    {
+    	return view('projects.list', compact('projects', 'category'));
     }
 }
