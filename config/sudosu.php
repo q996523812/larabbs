324238ef51608baf2bfb9b5241ed19bcbe2cc34f
qss <doublesan@126.com>
2020-03-13 06:36:30 +0000

    添加允许使用的顶级域名

--- config/sudosu.php
+++ config/sudosu.php
@@ -3,7 +3,7 @@
 return [
 
     // 允许使用的顶级域名
-    'allowed_tlds' => ['dev', 'local', 'test','com'],
+    'allowed_tlds' => ['dev', 'local', 'test','com','120.25.195.43'],
 
     // 用户模型
     'user_model' => App\Models\User::class
